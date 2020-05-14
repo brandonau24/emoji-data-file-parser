52ea9fe2a62223a86ed01e35931de521ac24a511
Brandon Au <tech.brandonau@gmail.com>
2020-05-14 17:12:35 -0400

    Logger does not print out undefined when an error object is not given.

--- src/Logger.js
+++ src/Logger.js
@@ -1,7 +1,11 @@
 export default class Logger {
 	static exitLog(exitCode, message, error) {
 		console.log(message);
-		console.log(error);
+
+		if (error) {
+			console.log(error);
+		}
+		
 		console.log('Exiting...');
 		process.exit(exitCode);
 	}
--- test/Logger.spec.js
+++ test/Logger.spec.js
@@ -7,10 +7,19 @@ chai.use(sinonChai);
 
 describe('Logger', () => {
 	describe('#exitLog', () => {
-		it('exits program and prints messages', () => {
-			const exitStub = sinon.stub(process, 'exit');
-			const logStub = sinon.stub(console, 'log');
+		let exitStub;
+		let logStub;
 
+		beforeEach(() => {
+			exitStub = sinon.stub(process, 'exit');
+			logStub = sinon.stub(console, 'log');
+		});
+
+		afterEach(() => {
+			sinon.restore();
+		});
+
+		it('exits program and prints messages', () => {
 			const exitCode = 1;
 			const message = 'error message';
 
@@ -21,9 +30,18 @@ describe('Logger', () => {
 			logStub.getCall(0).args[0].should.be.equal(message);
 			logStub.getCall(1).args[0].should.be.deep.equal({});
 			logStub.getCall(2).args[0].should.be.equal('Exiting...');
+		});
 
-			exitStub.restore();
-			logStub.restore();
+		it('does not try to print error object if none is given', () => {
+			const exitCode = 1;
+			const message = 'error message';
+
+			Logger.exitLog(exitCode, message);
+
+			exitStub.should.have.been.calledOnceWithExactly(exitCode);
+			logStub.should.have.been.calledTwice;
+			logStub.getCall(0).args[0].should.be.equal(message);
+			logStub.getCall(1).args[0].should.be.equal('Exiting...');
 		});
 	});
 });
